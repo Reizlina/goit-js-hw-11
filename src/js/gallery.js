6a65b40f2aa462735c5ead8430bb2ca9ceb0a23b
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-06-08 12:14:48 +0300

    Add style

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -77,19 +77,19 @@ function makeMarkup(hits) {
   </a>
         <div class="info">
           <p class="info-item">
-            <b>Likes</b>
+            <b>Likes:</b>
             ${hit.likes}
           </p>
           <p class="info-item">
-            <b>Views</b>
+            <b>Views:</b>
             ${hit.views}
           </p>
           <p class="info-item">
-            <b>Comments</b>
+            <b>Comments:</b>
             ${hit.comments}
           </p>
           <p class="info-item">
-            <b>Downloads</b>
+            <b>Downloads:</b>
             ${hit.downloads}
           </p>
         </div>
